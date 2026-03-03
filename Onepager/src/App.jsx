ae84c847435f89e8805caf1cf726deb70caf9090
StefanMuijs <145674829+StefanMuijs@users.noreply.github.com>
2026-03-03 11:49:51 +0100

    .

---
+++ Onepager/src/App.jsx
@@ -0,0 +1,25 @@
+import Header from "./Header.jsx";
+import Sprint0 from "./Sprint0.jsx";
+import Sprint1 from "./Sprint1.jsx";
+import Sprint2 from "./Sprint2.jsx";
+import Sprint3 from "./Sprint3.jsx";
+import Sprint4 from "./Sprint4.jsx";
+import Team1 from "./Team.jsx";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <main>
+                <Team1 />
+                <Sprint0 />
+                {/* <Sprint1 /> */}
+                {/* <Sprint2 /> */}
+                {/* <Sprint3 /> */}
+                {/* <Sprint4 /> */}
+            </main>
+        </>
+    );
+}
+
+export default App;
