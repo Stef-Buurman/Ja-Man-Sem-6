3b83d4d0e9ee75276247bd734248a94793c007ec
Stef Buurman <St3fbuurman@gmail.com>
2026-03-18 11:25:00 +0100

    feat: update encryption keys, enhance API documentation, and add TypeScript types for React

--- EncryptionHashingPrototype/Scripts/Node/dev.mjs
+++ EncryptionHashingPrototype/Scripts/Node/dev.mjs
@@ -1,8 +1,9 @@
 import { runCommandAsync } from "./Helpers/RunCommand.mjs";
-import { serverFolderName } from "./Helpers/Globals.mjs";
+import { clientFolderName, serverFolderName } from "./Helpers/Globals.mjs";
 import open from "open";
 
 runCommandAsync("dotnet watch run", serverFolderName);
+runCommandAsync("npm run dev", clientFolderName);
 
 setTimeout(() => {
     open("https://localhost:58895");
