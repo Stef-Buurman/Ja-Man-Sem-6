8637d47e272eef57da7efe992461590df0b38677
Stef Buurman <St3fbuurman@gmail.com>
2026-04-14 11:33:00 +0200

    fix: update .gitignore to include .db-wal and .db-journal files feat: add database update command to dev script

--- PrototypeProject/Scripts/Node/dev.mjs
+++ PrototypeProject/Scripts/Node/dev.mjs
@@ -2,6 +2,7 @@ import { runCommandAsync } from "./Helpers/RunCommand.mjs";
 import { clientFolderName, serverFolderName } from "./Helpers/Globals.mjs";
 import open from "open";
 
+runCommandAsync("dotnet ef database update", serverFolderName);
 runCommandAsync("dotnet watch run", serverFolderName);
 runCommandAsync("npm run dev", clientFolderName);
 
