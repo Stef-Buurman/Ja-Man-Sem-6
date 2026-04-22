443cc64fb711fc4bfba675af484f78015d7846b6
Stef Buurman <St3fbuurman@gmail.com>
2026-04-22 15:44:23 +0200

    feat: add docker compose stop command to stop services

--- NavigationPlatform/Scripts/Node/stop.mjs
+++ NavigationPlatform/Scripts/Node/stop.mjs
@@ -1,6 +1,8 @@
 import { safeRun } from "./Helpers/SafeRun.mjs";
 import { isWindows } from "./Helpers/Globals.mjs";
 
+safeRun("docker compose stop");
+
 if (isWindows) {
   safeRun("taskkill /IM dotnet.exe /F");
   safeRun("taskkill /IM node.exe /F");
