122c339eb62174213e07731a4f8a87125433351d
Stef Buurman <St3fbuurman@gmail.com>
2026-05-11 10:38:09 +0200

    fix: Update MAP_NORTH_OFFSET value in PathfindingMap component

--- NavigationPlatform/navigationplatform.client/src/components/PathfindingMap/PathfindingMap.tsx
+++ NavigationPlatform/navigationplatform.client/src/components/PathfindingMap/PathfindingMap.tsx
@@ -66,7 +66,7 @@ export const PathfindingMap: React.FC<PathfindingMapProps> = ({
     x: 400,
     y: 700,
   });
-  const MAP_NORTH_OFFSET = -25;
+  const MAP_NORTH_OFFSET = -99.5;
 
   useEffect(() => {
     startCompass();
