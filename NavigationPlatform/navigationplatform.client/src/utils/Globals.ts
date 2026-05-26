95a5332ef6f239513acc9c8a5cd0bff48a9df884
Stef Buurman <St3fbuurman@gmail.com>
2026-05-26 13:54:34 +0200

    feat: add empty line for improved code readability in Globals.ts

--- NavigationPlatform/navigationplatform.client/src/utils/Globals.ts
+++ NavigationPlatform/navigationplatform.client/src/utils/Globals.ts
@@ -2,3 +2,4 @@ export const UserLocationName = "Huidige locatie";
 export const EmergencyNodeName = "Nooduitgang";
 export const ToiletNodeName = "Toilet";
 export const CustomDestinationName = "Aangepaste Bestemming";
+
