dd930c1133ebb4d4c160021abc6c3b2fab5c3193
StefanMuijs <145674829+StefanMuijs@users.noreply.github.com>
2026-06-01 10:17:59 +0200

    Updated Heatmap

--- NavigationPlatform/navigationplatform.client/src/components/Layout/Layout.tsx
+++ NavigationPlatform/navigationplatform.client/src/components/Layout/Layout.tsx
@@ -10,18 +10,17 @@ export const Layout = ({ isAdmin }: { isAdmin: boolean }) => {
         <div className="max-w-[600px] mx-auto flex gap-2">
           <img src={settings} alt="Settings" className="max-h-6" />
           <h1 className="font-medium text-[#FFF0EA]">Gebouw</h1>
+          {isAdmin && (
+            <div className="">
+              <Link to="/admin/graph-editor" className="text-sm text-[#FFF0EA] hover:underline mr-4">
+                Graph Editor
+              </Link>
+              <Link to="/admin/heatmap-editor" className="text-sm text-[#FFF0EA] hover:underline">
+                Heatmap Editor
+              </Link>
+            </div>
+          )}
         </div>
-        <h1 className="font-medium text-[#FFF0EA]">Gebouw</h1>
-        {isAdmin && (
-          <div className="mt-2">
-            <Link to="/admin/graph-editor" className="text-sm text-[#FFF0EA] hover:underline mr-4">
-              Graph Editor
-            </Link>
-            <Link to="/admin/heatmap-editor" className="text-sm text-[#FFF0EA] hover:underline">
-              Heatmap Editor
-            </Link>
-          </div>
-        )}
       </nav>
 
       <main className="flex-1 bg-[#FFF0EA] py-6">
